80375b2994b5ed8b0a863d0f1b50085caf4db110
apk-1234 <81460947+apk-1234@users.noreply.github.com>
2022-05-11 16:49:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : ";
+  }
+function draw()
+{
+    image(video,0,0,380,380);
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+}
